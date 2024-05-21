b477b5fcc6aa2fc2b6cf16bf87bba307c8db2fff
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-21 17:53:57 +0000

    edit_file

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,4 +1,38 @@
-module.exports = {
+export enum Environment {
+  Browser = 'browser',
+  ES2020 = 'es2020',
+  Node = 'node',
+}
+
+export enum ReactVersion {
+  Version18_2 = '18.2',
+}
+
+export interface ESLintConfig {
+  root: boolean;
+  env: {
+    browser: boolean;
+    es2020: boolean;
+    node: boolean;
+  };
+  extends: string[];
+  ignorePatterns: string[];
+  parserOptions: {
+    ecmaVersion: string;
+    sourceType: string;
+  };
+  settings: {
+    react: {
+      version: ReactVersion;
+    };
+  };
+  plugins: string[];
+  rules: {
+    [key: string]: any;
+  };
+}
+
+const eslintConfig: ESLintConfig = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
@@ -9,7 +43,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: ReactVersion.Version18_2 } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -17,4 +51,6 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
+
+export default eslintConfig;
